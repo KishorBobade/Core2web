6909a296fd7b75aec7f87aff7df7248c4da220f1
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 01:52:22 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,7 +0,0 @@
-#Write  a program to find whether the number is dividible by 5 or not
-#input 55
-num=55
-if (num%5==0):
-    print(num,"is divisible by 5")
-else:
-    print(num,"is not divisible by 5")
