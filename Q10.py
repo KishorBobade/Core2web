f1c4e32f9c45b4287200b1004a653b0683aae919
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 01:52:51 +0530

    Delete Q10.py

--- Q10.py
+++
@@ -1,6 +0,0 @@
-#write the program that determines whether a given input year is a leap year or not
-year=int(input("Enter the year : "))
-if((year%100==0 and year%400==0)or year%4==0):
-    print("leap Year")
-else:
-    print("Not Leap Year")
