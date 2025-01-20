9f4a7caeb0c41fa052a0948dd8f48e60b5457df7
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 01:39:46 +0530

    If_else_Assignment_1

---
+++ Q6.py
@@ -0,0 +1,8 @@
+#Write  a program to check whether is character is Alphabet or not
+#input v
+input="v"
+input1=type(input)
+if (input1==str):
+    print(input,"is an Alphabet")
+else:
+    print(input,"is not an Alphabet")
---
+++ Q9.py
@@ -0,0 +1,17 @@
+#Write  a program to check whether the input character is a vowel or consonant
+#input "a" #vowel
+#input "b" #condonant
+char=input("Enter the character : ")
+if (char=="a"):
+    print("vowel")
+elif(char=="e"):
+    print("vowel")
+elif (char=="i"):
+    print("vowel")
+elif(char=="o"):
+    print("vowel")
+elif(char=="u"):
+    print("vowel")
+else:
+    print("consonant")
+    
