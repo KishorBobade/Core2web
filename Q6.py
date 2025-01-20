4ed0ce85f303d9b8d9329e27b1d6e49e0c2641db
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 01:51:38 +0530

    Delete Q6.py

--- Q6.py
+++
@@ -1,8 +0,0 @@
-#Write  a program to check whether is character is Alphabet or not
-#input v
-input="v"
-input1=type(input)
-if (input1==str):
-    print(input,"is an Alphabet")
-else:
-    print(input,"is not an Alphabet")
