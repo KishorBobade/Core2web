b32e876b23960a7e5b5dadcd1f18c346afa91c31
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 01:52:04 +0530

    Delete Q2.py

--- Q2.py
+++
@@ -1,9 +0,0 @@
-#Write the program to check whether the number is Negative, positive, or equal to zero.
-#input -2
-num=-2
-if (num>0):
-    print(num,"is the Positive Number")
-elif(0>num):
-    print(num,"is Negative number")
-else:
-    print(num ,"Number are Equal zero.")
