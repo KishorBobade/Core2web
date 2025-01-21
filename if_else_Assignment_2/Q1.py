b573168b01d7791bf60e584fec0ece9e9e41892c
Kishor Patil <135812528+KishorBobade@users.noreply.github.com>
2025-01-21 12:31:03 +0530

    Add files via upload

--- if_else_Assignment_2/Q1.py
+++ if_else_Assignment_2/Q1.py
@@ -1,1 +1,9 @@
-
+#Write a program to find a maximum between two numbers 
+num1=1
+num2=2
+if (num1>num2):
+    print(num1,"is Max number between 1 & 2")
+elif(num1<num2):
+    print(num2,"is Max number between 1 & 2")
+else:
+    print(num1 ,"and", num2 ,"are Equal numbers.")
---
+++ if_else_Assignment_2/Q3.py
@@ -0,0 +1,7 @@
+#Write  a program to find whether the number is even of odd
+#input 4
+num=-2
+if (num%2==0):
+    print(num,"is an Even Number")
+else:
+    print(num,"is an Odd number")
